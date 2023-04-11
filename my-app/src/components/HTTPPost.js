b212a0fecd58f8ad317fe2e95c0a27e787feb8fd
NickdevC <nick_coles@msn.com>
2023-04-11 10:06:38 +0000

    Add HTTPPost.js file

---
+++ my-app/src/components/HTTPPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HTTPPost extends Component {
+  render() {
+    return (
+      <div>
+        It works!
+      </div>
+    )
+  }
+}
+
+export default HTTPPost
